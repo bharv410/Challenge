512d386a8cc92049914f0c04c28a04ca1925db94
Benjamin Harvey <bharv410@users.noreply.github.com>
2016-08-14 10:19:45 -0700

    correct url

--- app/src/main/java/challenge/skurt/com/skurtchallenge/FlightStatsService.java
+++ app/src/main/java/challenge/skurt/com/skurtchallenge/FlightStatsService.java
@@ -1,14 +1,12 @@
 package challenge.skurt.com.skurtchallenge;
 
-import java.util.List;
-
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface FlightStatsService {
 
-    @GET("flex/flightstatus/rest/v2/json/airport/status/OKBK/dep/2013/2/27/13?appId=16decbe1&appKey=2ed0ada41c7ef6c31253ce11c93b8692&utc=false&numHours=6&maxFlights=10")
+    @GET("flex/flightstatus/rest/v2/json/flight/status/761435187?appId=91b929e6&appKey=2eebba75c50ce13c31b9ef0b331fb93a")
     Call<ResponseBody> getFlightStats();
 
 }
